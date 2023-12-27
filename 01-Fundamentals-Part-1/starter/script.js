1e0ae0a1c607ca77ce75d691d0173711cf854675
ctrlshftejct <harveyap20@gmail.com>
2023-12-26 19:21:39 -0500

    data type and assignment

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -15,3 +15,30 @@ let myCurrentJob = 'System Administrator'
 
 console.log(myFirstJob);
 console.log(mySecondJob);
+
+// object
+let me = {
+    name: 'Andrew'
+};
+
+// 7 primitive data types
+
+// number: floating point numbers > used for decimals and integers
+let age = 23;
+
+// string: sequence of characters > used for text
+let lastName = 'Harvey';
+
+// boolean: logical type that can only be true or false > used for taking decisions
+let fullAge = true;
+
+// undefined: value taken by a variable that is not yet defined ('empty value')
+let children;
+
+// null: also means 'empty value'
+
+// Symbol (ES2015): value that is unique and cannot be changed [not useful for now]
+
+//BigInt (ES2020): larger integers than the Number type can hold
+
+// JavaScript has dynamic typing, maybe we do not manually have to define the data type, they are determined automatically.
