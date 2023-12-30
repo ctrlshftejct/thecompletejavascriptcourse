9d5603450e1d62b502ab7f11d4f8bf0ad2a7d990
ctrlshftejct <harveyap20@gmail.com>
2023-12-29 22:53:01 -0500

    friday studies

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,3 +1,4 @@
+/*
 let js = 'amazing';
 console.log(40 + 8 + 23 - 10);
 
@@ -55,3 +56,79 @@ console.log(typeof lastName);
 
 // do not need let after variable has already been "declared"
 javascriptIsFun = 'YES!';
+*/
+
+/*
+
+// 2.12 DECLARING VARIABLES
+
+// let, const, var
+// let is newer with const, var is older
+
+let age = 30;
+age = 31; // you can declare with let at one point then 'reassign' value later
+// also known as mutate it
+
+const birthYear = 1987; // good example because birth year can't change
+//const must have an initial value
+
+var job = 'programmer';
+job = 'coder';
+// var is older version of 'let', most people shouldn't use it
+
+// below the surface 'let' and 'var' are different under the hood
+
+// ALWAYS PROPERLY DECLARE VARIABLES, even though you don't need to
+
+// 2.13 - BASIC OPERATORS
+
+// const ageAndrew = 2023 - 1987;
+// console.log(ageAndrew);
+
+// const ageCallie = 2023 - 1992;
+// console.log(ageCallie);
+
+// Math Operators
+const currentYear = 2023
+const ageAndrew = currentYear - 1987;
+const ageCallie = currentYear - 1992;
+console.log(ageCallie, ageAndrew);
+
+console.log(ageAndrew * 2, ageAndrew / 2, ageAndrew ** 2);
+// ** = to the power of = exponents
+
+// concatenate
+const firstName = 'andrew';
+const lastName = 'harvey';
+console.log(firstName + lastName);
+console.log(firstName + ' ' + lastName);
+// template strings are better at doing above, will learn later
+
+// ASSIGNMENT OPERATORS
+let x = 10 + 5;
+console.log(x);
+// equal sign
+
+x += 10; // x = x + 10
+console.log(x);
+
+x -= 10; // x = x - 10
+console.log(x);
+
+x *= 4; // x = x * 4
+console.log(x);
+
+x++; // x = x + 1
+// good for counters
+console.log(x);
+
+x--; // x = x - 1
+console.log(x);
+
+// COMPARISON OPERATORS
+console.log(ageAndrew > ageCallie);
+// boolean comparison, equals true
+
+*/
+
+// 2.14 OPERATOR PRECEDENCE
