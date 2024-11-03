66de292b4b29851a9124283cdfb9655b476283f6
ctrlshftejct <harveyap20@gmail.com>
2024-11-03 07:33:40 -0500

    updated

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,31 @@
+// // BOOLEAN LOGIC
+
+// // const hasDriversLicense = true; // variable A
+// // const hasGoodVision = true; // variable B
+
+// // console.log(hasDriversLicense && hasGoodVision); // && = AND
+// // console.log(hasDriversLicense || hasGoodVision); // || = OR
+// // console.log(!hasDriversLicense);
+
+// // // const shouldDrive = hasDriversLicense && hasGoodVision
+
+// // // if(shouldDrive) {
+// // //     console.log('Sarah is able to drive!');
+// // // } else {
+// // //     console.log('Someone else should drive...');
+// // // }
+
+// const isTired = true; // variable C
+// console.log(hasDriversLicense || hasGoodVision || isTired);
+
+// const shouldDrive = hasDriversLicense && hasGoodVision && !isTired
+
+// if(shouldDrive) {
+//     console.log('Sarah is able to drive!');
+// } else {
+//     console.log('Someone else should drive...');
+// }
+
+// THE SWITCH STATEMENT
+
+const day = 'monday';
